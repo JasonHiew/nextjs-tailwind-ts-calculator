5e6afc9807ffa14f6d13c3980c73ece33535f80c
Jason Hiew <jasonhiew.123jh@gmail.com>
2022-05-26 01:58:29 +0800

    fixed (more like ignored...) some typescript es-lint issues

--- src/components/calculator/CalculatorDisplay.tsx
+++ src/components/calculator/CalculatorDisplay.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 type Props = {
-  display: string;
+  display: string | null | undefined;
   children: React.ReactNode;
 };
 
--- src/components/calculator/CalculatorRoot.tsx
+++ src/components/calculator/CalculatorRoot.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as React from 'react';
 
 import CalculatorDisplay from '@/components/calculator/CalculatorDisplay';
@@ -6,10 +8,14 @@ import CalculatorKeys from '@/components/calculator/CalculatorKeys';
 //reference guide. it's in javascript but i'm converting it to typescript + react
 //https://www.freecodecamp.org/news/how-to-build-an-html-calculator-app-from-scratch-using-javascript-4454b8714b98
 const CalculatorRoot: React.FC = () => {
-  const [display, setDisplay] = React.useState<string>('0');
-  const [previousKeyType, setPreviousKeyType] = React.useState<string>('');
-  const [firstValue, setFirstValue] = React.useState<string>('');
-  const [operator, setOperator] = React.useState<string>('');
+  const [display, setDisplay] = React.useState<string | null | undefined>('0');
+  const [previousKeyType, setPreviousKeyType] = React.useState<
+    string | null | undefined
+  >('');
+  const [firstValue, setFirstValue] = React.useState<string | null | undefined>(
+    ''
+  );
+  const [operator, setOperator] = React.useState<string | null | undefined>('');
   const displayBoxRef = React.useRef<HTMLDivElement>(null);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -42,17 +48,21 @@ const CalculatorRoot: React.FC = () => {
         setDisplay('0');
       }
 
-      const calculate = (n1: string, op: string, n2: string) => {
+      const calculate = (
+        n1: string | null | undefined,
+        op: string | null | undefined,
+        n2: string | null | undefined
+      ) => {
         let result = 0;
 
         if (op === 'add') {
-          result = parseFloat(n1) + parseFloat(n2);
+          result = parseFloat(n1 || '') + parseFloat(n2 || '');
         } else if (op === 'subtract') {
-          result = parseFloat(n1) - parseFloat(n2);
+          result = parseFloat(n1 || '') - parseFloat(n2 || '');
         } else if (op === 'multiply') {
-          result = parseFloat(n1) * parseFloat(n2);
+          result = parseFloat(n1 || '') * parseFloat(n2 || '');
         } else if (op === 'divide') {
-          result = parseFloat(n1) / parseFloat(n2);
+          result = parseFloat(n1 || '') / parseFloat(n2 || '');
         }
 
         return result.toString();
@@ -73,7 +83,7 @@ const CalculatorRoot: React.FC = () => {
         } else {
           //if the displayedNum is not 0 and the previousKeyType is not operator, then append the keyContent to the displayedNum
           // displayBoxRef.current.textContent = displayedNum + keyContent; //useRef way
-          setDisplay(displayedNum + keyContent); //useState way
+          setDisplay(displayedNum! + keyContent); //useState way
         }
         setPreviousKeyType('number');
       }
