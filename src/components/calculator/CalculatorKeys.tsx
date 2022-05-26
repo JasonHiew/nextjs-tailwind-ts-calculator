bac3efc02245db605941f8a4d10a5175b9d75a1b
Jason Hiew <jasonhiew.123jh@gmail.com>
2022-05-26 12:44:06 +0800

    completed edge case handling (some bugs left), started refactoring the handleClick() function

--- src/components/calculator/CalculatorKeys.tsx
+++ src/components/calculator/CalculatorKeys.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 type Props = {
   handleClick: (keyType: React.MouseEvent<HTMLButtonElement>) => void;
+  clearMode: string | null | undefined;
 };
 
 const CalculatorKeys: React.FC<Props> = (props) => {
@@ -10,112 +11,112 @@ const CalculatorKeys: React.FC<Props> = (props) => {
       <div className='grid w-80 grid-cols-4 gap-1 rounded-xl'>
         <button
           data-action='add'
-          className='bg-primary-400 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-primary-400 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           +
         </button>
         <button
           data-action='subtract'
-          className='bg-primary-400 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-primary-400 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           -
         </button>
         <button
           data-action='multiply'
-          className='bg-primary-400 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-primary-400 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           ×
         </button>
         <button
           data-action='divide'
-          className='bg-primary-400 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-primary-400 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           ÷
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           7
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           8
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           9
         </button>
         <button
           data-action='calculate'
-          className='row-span-4 bg-orange-500 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='row-span-4 bg-orange-500 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           =
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           4
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           5
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           6
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           1
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           2
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           3
         </button>
         <button
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           0
         </button>
         <button
           data-action='decimal'
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
           .
         </button>
         <button
           data-action='clear'
-          className='bg-amber-300 text-4xl font-bold transition-transform duration-200 ease-in-out hover:scale-105 active:scale-90'
+          className='bg-amber-300 text-4xl font-bold transition-transform duration-100 ease-out hover:scale-95 active:scale-90'
           onClick={(e) => props.handleClick(e)}
         >
-          AC
+          {props.clearMode}
         </button>
       </div>
     </>
--- src/components/calculator/CalculatorRoot.tsx
+++ src/components/calculator/CalculatorRoot.tsx
@@ -16,7 +16,56 @@ const CalculatorRoot: React.FC = () => {
     ''
   );
   const [operator, setOperator] = React.useState<string | null | undefined>('');
+  const [modValue, setModValue] = React.useState<string | null | undefined>('');
+  const [clearMode, setClearMode] = React.useState<string | null | undefined>(
+    'AC'
+  );
   const displayBoxRef = React.useRef<HTMLDivElement>(null);
+
+  const calculate = (
+    n1: string | null | undefined,
+    op: string | null | undefined,
+    n2: string | null | undefined
+  ) => {
+    const firstNum = parseFloat(n1 || '');
+    const secondNum = parseFloat(n2 || '');
+    if (op === 'add') {
+      return firstNum + secondNum;
+    }
+
+    if (op === 'subtract') {
+      return firstNum - secondNum;
+    }
+
+    if (op === 'multiply') {
+      return firstNum * secondNum;
+    }
+
+    if (op === 'divide') {
+      return firstNum / secondNum;
+    }
+  };
+
+  const resetStates = (): void => {
+    setFirstValue('');
+    setModValue('');
+    setOperator('');
+    setPreviousKeyType('');
+    // setDisplay('0');
+  };
+
+  const getKeyType = (key: any): string | null | undefined => {
+    const { action } = key.dataset;
+    if (!action) return 'number';
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    )
+      return 'operator';
+    return action;
+  };
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (e.currentTarget.matches('button')) {
@@ -25,6 +74,23 @@ const CalculatorRoot: React.FC = () => {
       const keyContent = key.textContent; //innerText vs textContent: https://stackoverflow.com/questions/12093774/what-is-the-difference-between-innertext-and-textcontent
       const displayedNum = displayBoxRef.current?.textContent;
 
+      const addToggledButtonStyling = (): void => {
+        // button styling for operator buttons when clicked
+        key.classList.add('scale-90');
+        Array.from(key.parentNode.children).forEach((k) =>
+          k.classList.remove('hover:scale-95')
+        );
+      };
+
+      const removeToggledButtonStyling = (): void => {
+        // button styling for operator buttons when clicked
+        key.classList.add('hover:scale-95');
+        Array.from(key.parentNode.children).forEach((k) => {
+          k.classList.remove('scale-90');
+        });
+      };
+
+      // User presses operator key
       if (
         action === 'add' ||
         action === 'subtract' ||
@@ -34,58 +100,90 @@ const CalculatorRoot: React.FC = () => {
         setPreviousKeyType('operator');
         setFirstValue(displayedNum);
         setOperator(action);
+
+        const firstVal = firstValue;
+        const op = operator;
+        const secondVal = displayedNum;
+
+        if (
+          firstVal &&
+          op &&
+          previousKeyType !== 'operator' &&
+          previousKeyType !== 'calculate'
+        ) {
+          const calcValue = calculate(firstVal, op, secondVal);
+          setFirstValue(calcValue);
+          setDisplay(calcValue);
+        } else {
+          setFirstValue(displayedNum);
+        }
+
+        addToggledButtonStyling();
         console.log('operator key!');
       }
 
       if (action === 'decimal') {
-        setDisplay(displayedNum + '.');
+        if (!displayedNum?.includes('.')) {
+          setDisplay(displayedNum + '.');
+        } else if (
+          previousKeyType === 'operator' ||
+          previousKeyType === 'calculate'
+        ) {
+          setDisplay('0.');
+        }
+        setPreviousKeyType('decimal');
+        // removeToggledButtonStyling();
+      }
+
+      if (action !== 'clear') {
+        setClearMode('CE');
       }
 
       if (action === 'clear') {
-        setPreviousKeyType('');
-        setFirstValue('');
-        setOperator('');
-        setDisplay('0');
+        if (clearMode === 'AC') {
+          resetStates();
+          setPreviousKeyType('clear');
+        } else {
+          setDisplay('0');
+          setClearMode('AC');
+        }
+        removeToggledButtonStyling();
+        setPreviousKeyType('clear');
       }
 
-      const calculate = (
-        n1: string | null | undefined,
-        op: string | null | undefined,
-        n2: string | null | undefined
-      ) => {
-        let result = 0;
+      if (action === 'calculate') {
+        let firstVal = firstValue;
+        const op = operator;
+        let secondVal = displayedNum;
 
-        if (op === 'add') {
-          result = parseFloat(n1 || '') + parseFloat(n2 || '');
-        } else if (op === 'subtract') {
-          result = parseFloat(n1 || '') - parseFloat(n2 || '');
-        } else if (op === 'multiply') {
-          result = parseFloat(n1 || '') * parseFloat(n2 || '');
-        } else if (op === 'divide') {
-          result = parseFloat(n1 || '') / parseFloat(n2 || '');
+        if (firstValue) {
+          if (previousKeyType === 'calculate') {
+            firstVal = displayedNum;
+            secondVal = modValue;
+
+            setFirstValue(displayedNum);
+          }
+
+          setDisplay(calculate(firstVal, op, secondVal));
         }
 
-        return result.toString();
-      };
-
-      if (action === 'calculate') {
-        console.log(firstValue, operator, displayedNum);
-        const result = calculate(firstValue, operator, displayedNum);
-
-        setDisplay(result);
+        setModValue(secondVal);
+        setPreviousKeyType('calculate');
       }
 
       if (!action) {
-        if (displayedNum === '0' || previousKeyType === 'operator') {
-          //if the current displayedNum is 0 or previousKeyType is operator, then replace the displayedNum to the keyContent
-          // displayBoxRef.current.textContent = keyContent; //useRef way
-          setDisplay(keyContent); //useState way
+        if (
+          displayedNum === '0' ||
+          previousKeyType === 'operator' ||
+          previousKeyType === 'calculate'
+        ) {
+          setDisplay(keyContent);
         } else {
-          //if the displayedNum is not 0 and the previousKeyType is not operator, then append the keyContent to the displayedNum
-          // displayBoxRef.current.textContent = displayedNum + keyContent; //useRef way
-          setDisplay(displayedNum! + keyContent); //useState way
+          setDisplay(displayedNum! + keyContent); //in TypeScript, adding ! to the end of a variable means that it's not null or undefined
         }
         setPreviousKeyType('number');
+
+        removeToggledButtonStyling();
       }
 
       // console.log(`Key: ${key}`);
@@ -115,7 +213,7 @@ const CalculatorRoot: React.FC = () => {
       <CalculatorDisplay ref={displayBoxRef} display={display}>
         0
       </CalculatorDisplay>
-      <CalculatorKeys handleClick={handleClick} />
+      <CalculatorKeys handleClick={handleClick} clearMode={clearMode} />
     </>
   );
 };
